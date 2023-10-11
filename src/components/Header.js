3cfc2374e783f9b5d39a95f9fc4797aea82cd5c6
Rico <v_fharsono@tencent.com>
2023-10-11 15:07:43 +0800

    edit to tablets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import useWindows from "../hooks/use-windows";
 import styles from "./Header.module.css";
 
 import patternRing from "../assets/images/pattern-rings.svg";
@@ -8,23 +10,27 @@ import frontendmentor from "../assets/images/icon-frontend-mentor.svg";
 import linkedin from "../assets/images/icon-linkedin.svg";
 import twitter from "../assets/images/icon-twitter.svg";
 
+import photoDesktop from "../assets/images/image-profile-desktop.webp";
+import photoTablet from "../assets/images/image-profile-tablet.webp";
+import photoMobile from "../assets/images/image-profile-mobile.webp";
+
 const Header = () => {
-  const photoHelper = "image-profile-desktop.webp";
   return (
     <>
       <header>
         <div className={styles.images}>
           <img className={styles.ring} src={patternRing} alt="" />
+          {/* <img
+            className={styles.profilePhoto}
+            src={require(`../assets/images/${photoHelper}`)}
+            alt=""
+          /> */}
 
-          <picture className={styles.profilePhoto}>
-            <source media="(max-width:768px)" srcset="img_pink_flowers.jpg" />
-            <source media="(max-width:375px)" srcset="img_white_flower.jpg" />
-            <img
-              src={require(`../assets/images/${photoHelper}`)}
-              alt=""
-              style={{ width: 445 }}
-            />
-          </picture>
+          {/* <picture className={styles.profilePhoto}>
+            <source media="(max-width:1024px)" srcset={photoTablet} />
+            <source media="(max-width:425px)" srcset={photoMobile} />
+            <img src={photoDesktop} alt="" style={{ width: 445 }} />
+          </picture> */}
           <img className={styles.circle} src={patternCircle} alt="" />
         </div>
         <nav className={styles.headerNav}>
@@ -48,11 +54,9 @@ const Header = () => {
         </nav>
 
         <div className={styles.mainHeader}>
-          <h1>Nice to meet you!</h1>
           <h1>
-            I'm <span>Adam Keyes</span>.
+            Nice to meet you! I'm <span>Adam Keyes</span>.
           </h1>
-
           <p>
             Based in the UK, I'm a front-end developer passionate about building
             accessible web apps that users love.
